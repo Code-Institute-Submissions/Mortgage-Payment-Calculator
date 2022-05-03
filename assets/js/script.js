fc89ef24c11aa23ab96e796b5f7de022c18b02fa
tomf247 <tomfoley247@gmail.com>
2022-05-03 21:58:26 +0000

    Add spacing and comments to JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,39 @@
 const context = document.getElementById("data-set").getContext("2d");
 const calcButton = document.querySelector(".input-group button");
 let line = new Chart(context, {});
-
 let data = [];
 let labels = [];
 
 calcButton.addEventListener("click", calculate);
 
+
 function calculate(e) {
   e.preventDefault();
+
   const loanAmountInput = document.getElementById("loan-amount");
   const interestRateInput = document.getElementById("interest-rate");
   const loanTenureInput = document.getElementById("loan-tenure");
 
-  //let interest = interestRate / 12 / 100;
   let extra = 0.0;
   i = interestRateInput.value / 100;
   loan_amt = loanAmountInput.value;
   months = loanTenureInput.value;
   rate = interestRateInput.value;
 
+  /* Validate User Input */
+
   let regexNumber = /^[0-9]*(\.[0-9]{0,2})?$/;
   if (!loan_amt.match(regexNumber)) {
     loanAmountInput.value = "10000";
   }
+
+  /* Calculate Monthly Payment, including principal and interest */
+
   var monthly_payment =
     (loan_amt * (i / 12) * Math.pow(1 + i / 12, months)) /
     (Math.pow(1 + i / 12, months) - 1);
+
+  /* Output a table of loan summary calculations to "results section" of HTML page */
 
   var info = "";
   info += "<table class='results'>";
